adcb15a3d410817487607b7698ce24e77f92fba1
Prathmesh <65167693+prathmeshjagtap@users.noreply.github.com>
2020-09-15 13:31:35 +0530

    Add files via upload

---
+++ build/precache-manifest.acf3d368e09b91365443dae644c1d3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4f5b258a0e2e7cc95f0f6fa51a6474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa5c09f3a371986ae13",
+    "url": "/static/css/main.5cad86b4.chunk.css"
+  },
+  {
+    "revision": "1e8326e9dd538634541d",
+    "url": "/static/js/2.a9a3c516.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.a9a3c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa5c09f3a371986ae13",
+    "url": "/static/js/main.d4a0ca19.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "2cd6a26170cb9b78eb04ee871640f64e";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchAnime: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchFantasy: `/discover/movie?api_key=${API_KEY}&with_genres=14`,
+};
+
+export default requests;
